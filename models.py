b6ba4adaaba16fe5b340cc14c6a6ce94e30efcfd
Andrea Michelle <andreagon6135@gmail.com>
2022-10-23 18:30:08 -0400

    Rewritten Files that Included SQL relationships

--- models.py
+++ models.py
@@ -6,11 +6,11 @@ import datetime
 #BOILER PLATE CODE TO INCLUDE THE DATABSE IN FILE
 db = SQLAlchemy()
 
+
 def connect_db(app):
     """Connect this database to provided Flask app."""
     db.app = app
     db.init_app(app)
-
 #MODELS GO BELOW
 class User(db.Model):
     """User"""
@@ -46,8 +46,30 @@ class Post(db.Model):
         default=datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
+    # tags = db.relationship("PostTag", backref="Post")
+
     @property
     def friendly_date(self):
         """Return nicely-formatted date."""
 
         return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+class Tag(db.Model):
+    """Tag to add on posts"""
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship("Post", secondary="posts_tags", backref="tags")
+
+
+class PostTag(db.Model):
+    """Tag on post."""
+
+    __tablename__ = "posts_tags"
+
+    # id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True, nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True, nullable=False)
